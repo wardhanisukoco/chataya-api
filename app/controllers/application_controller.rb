bfcc987930521a17aef3190553cc1a235023a78a
Wardhani Sukoco <wardhanisukoco@gmail.com>
2022-10-25 15:57:34 +0700

    fix: cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@ class ApplicationController < ActionController::API
     require 'securerandom'
     include ActionController::Cookies
     def session_id
-        cookies[:session_id] ||= {
-            value: SecureRandom.uuid,
+        cookies[:session_id] ||= SecureRandom.uuid 
+        cookies[:session_id] = {
+            value: cookies[:session_id],
             same_site: :none,
             secure: true
-        } 
+        }
     end
 end
