503a4ca82fae5d3baac12c6fab4aae1d32d02b7b
Wardhani Sukoco <wardhanisukoco@gmail.com>
2022-10-25 14:20:37 +0700

    fix: cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,4 +12,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         methods: %i(get post put patch delete options head),
         credentials: true
     end
+    allow do
+      origins 'https://chataya-api.herokuapp.com/rooms'
+      resource '*',
+        headers: :any,
+        methods: %i(get post put patch delete options head),
+        credentials: true
+    end
 end
